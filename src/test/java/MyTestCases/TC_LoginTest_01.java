ee88fa3ebbb996baab261df3f0ace982a460a865
manu00-droid <manavsidana00@gmail.com>
2023-04-23 01:53:16 +0530

    feat: added logger in TC_LoginTest_01.java

--- src/test/java/MyTestCases/TC_LoginTest_01.java
+++ src/test/java/MyTestCases/TC_LoginTest_01.java
@@ -1,5 +1,7 @@
 package MyTestCases;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,14 +11,23 @@ public class TC_LoginTest_01 extends BaseClass {
 
 	@Test
 	public void loginTest() {
+		Logger logger;
+		logger = LogManager.getLogger(TC_LoginTest_01.class);
 		driver.get(baseURL);
+		logger.info("URL is opened");
 		LoginPage lp = new LoginPage(driver);
-		lp.login(username, password);
-//		Assert.assertTrue(true);
+		lp.setEmailId(username);
+		logger.info("email id is entered");
+		lp.setPassword(password);
+		logger.info("password is entered");
+		lp.clickSubmit();
+		logger.info("sign in button is clicked");
 		if (driver.getTitle().equals("My Account")) {
 			Assert.assertTrue(true);
+			logger.info("sign in was successful");
 		} else {
 			Assert.assertTrue(false);
+			logger.info("sign was not successful");
 		}
 	}
 }
